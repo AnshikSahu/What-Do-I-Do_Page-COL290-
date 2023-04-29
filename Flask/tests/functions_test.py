6454b10100686d35520be2fdd016929f27618599
AB271202 <anishbanerjee2002@gmail.com>
2023-04-29 21:59:16 +0530

    Trying tests

--- Flask/tests/functions_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import functions
-
-class Test_Movies(unittest.TestCase):
-    def test_movie_details_full_non_existing_movies(self):
-        with self.assertRaises(ValueError):
-            movie=functions.movie_details_full("1234")
